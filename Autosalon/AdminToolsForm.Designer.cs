9047303ad3ee718bacf8d18d9ecfad89b291a037
povser75@outlook.com <povser75@outlook.com>
2025-12-12 19:00:46 +0400

    Удаление запчастей

--- Autosalon/AdminToolsForm.Designer.cs
+++ Autosalon/AdminToolsForm.Designer.cs
@@ -29,23 +29,28 @@
         private void InitializeComponent()
         {
             this.AddPanel = new System.Windows.Forms.Panel();
+            this.addButton = new System.Windows.Forms.Button();
+            this.opisTextBox = new System.Windows.Forms.TextBox();
+            this.loadpicButton = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.carComboBox = new System.Windows.Forms.ComboBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.DelPanel = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.nameTextBox = new System.Windows.Forms.TextBox();
-            this.carComboBox = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.priceTextBox = new System.Windows.Forms.TextBox();
-            this.picBox = new System.Windows.Forms.PictureBox();
-            this.loadpicButton = new System.Windows.Forms.Button();
-            this.opisTextBox = new System.Windows.Forms.TextBox();
-            this.addButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.DelButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DelComboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.AddPanel.SuspendLayout();
-            this.DelPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.DelPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // AddPanel
@@ -68,66 +73,43 @@
             this.AddPanel.Size = new System.Drawing.Size(1060, 455);
             this.AddPanel.TabIndex = 0;
             // 
-            // label1
+            // addButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Добавить комплект";
+            this.addButton.Location = new System.Drawing.Point(814, 407);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(224, 42);
+            this.addButton.TabIndex = 10;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
-            // DelPanel
+            // opisTextBox
             // 
-            this.DelPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.DelPanel.Controls.Add(this.label2);
-            this.DelPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DelPanel.Location = new System.Drawing.Point(0, 455);
-            this.DelPanel.Name = "DelPanel";
-            this.DelPanel.Size = new System.Drawing.Size(1060, 164);
-            this.DelPanel.TabIndex = 1;
+            this.opisTextBox.Location = new System.Drawing.Point(706, 36);
+            this.opisTextBox.Multiline = true;
+            this.opisTextBox.Name = "opisTextBox";
+            this.opisTextBox.Size = new System.Drawing.Size(342, 310);
+            this.opisTextBox.TabIndex = 9;
             // 
-            // label2
+            // loadpicButton
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 17);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Удалить комплект";
+            this.loadpicButton.Location = new System.Drawing.Point(338, 352);
+            this.loadpicButton.Name = "loadpicButton";
+            this.loadpicButton.Size = new System.Drawing.Size(350, 34);
+            this.loadpicButton.TabIndex = 8;
+            this.loadpicButton.Text = "Загрузить картинку";
+            this.loadpicButton.UseVisualStyleBackColor = true;
+            this.loadpicButton.Click += new System.EventHandler(this.loadpicButton_Click);
             // 
-            // nameTextBox
+            // picBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(27, 204);
-            this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(290, 28);
-            this.nameTextBox.TabIndex = 1;
-            // 
-            // carComboBox
-            // 
-            this.carComboBox.FormattingEnabled = true;
-            this.carComboBox.Location = new System.Drawing.Point(27, 109);
-            this.carComboBox.Name = "carComboBox";
-            this.carComboBox.Size = new System.Drawing.Size(286, 30);
-            this.carComboBox.TabIndex = 2;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(180, 22);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Выбери автомобиль";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 169);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(191, 22);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Введи наименование";
+            this.picBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBox.Location = new System.Drawing.Point(338, 36);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(350, 310);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 7;
+            this.picBox.TabStop = false;
             // 
             // label5
             // 
@@ -145,47 +127,123 @@
             this.priceTextBox.Size = new System.Drawing.Size(290, 28);
             this.priceTextBox.TabIndex = 5;
             // 
-            // picBox
+            // label4
             // 
-            this.picBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picBox.Location = new System.Drawing.Point(338, 36);
-            this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(350, 310);
-            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picBox.TabIndex = 7;
-            this.picBox.TabStop = false;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Введи наименование";
             // 
-            // loadpicButton
+            // label3
             // 
-            this.loadpicButton.Location = new System.Drawing.Point(338, 352);
-            this.loadpicButton.Name = "loadpicButton";
-            this.loadpicButton.Size = new System.Drawing.Size(350, 34);
-            this.loadpicButton.TabIndex = 8;
-            this.loadpicButton.Text = "Загрузить картинку";
-            this.loadpicButton.UseVisualStyleBackColor = true;
-            this.loadpicButton.Click += new System.EventHandler(this.loadpicButton_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Выбери автомобиль";
             // 
-            // opisTextBox
+            // carComboBox
             // 
-            this.opisTextBox.Location = new System.Drawing.Point(706, 36);
-            this.opisTextBox.Multiline = true;
-            this.opisTextBox.Name = "opisTextBox";
-            this.opisTextBox.Size = new System.Drawing.Size(342, 310);
-            this.opisTextBox.TabIndex = 9;
+            this.carComboBox.FormattingEnabled = true;
+            this.carComboBox.Location = new System.Drawing.Point(27, 109);
+            this.carComboBox.Name = "carComboBox";
+            this.carComboBox.Size = new System.Drawing.Size(286, 30);
+            this.carComboBox.TabIndex = 2;
             // 
-            // addButton
+            // nameTextBox
             // 
-            this.addButton.Location = new System.Drawing.Point(814, 407);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(224, 42);
-            this.addButton.TabIndex = 10;
-            this.addButton.Text = "Добавить";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            this.nameTextBox.Location = new System.Drawing.Point(27, 204);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(290, 28);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавить комплект";
+            // 
+            // DelPanel
+            // 
+            this.DelPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DelPanel.Controls.Add(this.DelButton);
+            this.DelPanel.Controls.Add(this.label8);
+            this.DelPanel.Controls.Add(this.textBox1);
+            this.DelPanel.Controls.Add(this.DelComboBox1);
+            this.DelPanel.Controls.Add(this.label7);
+            this.DelPanel.Controls.Add(this.label2);
+            this.DelPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DelPanel.Location = new System.Drawing.Point(0, 455);
+            this.DelPanel.Name = "DelPanel";
+            this.DelPanel.Size = new System.Drawing.Size(1060, 164);
+            this.DelPanel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Удалить комплект";
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // DelButton
+            // 
+            this.DelButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.DelButton.Location = new System.Drawing.Point(776, 88);
+            this.DelButton.Name = "DelButton";
+            this.DelButton.Size = new System.Drawing.Size(248, 30);
+            this.DelButton.TabIndex = 11;
+            this.DelButton.Text = "Удалить";
+            this.DelButton.UseVisualStyleBackColor = false;
+            this.DelButton.Click += new System.EventHandler(this.DelButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(388, 63);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(314, 22);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Выбранный компдект для удаления";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(370, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 30);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DelComboBox1
+            // 
+            this.DelComboBox1.FormattingEnabled = true;
+            this.DelComboBox1.Location = new System.Drawing.Point(16, 89);
+            this.DelComboBox1.Name = "DelComboBox1";
+            this.DelComboBox1.Size = new System.Drawing.Size(322, 30);
+            this.DelComboBox1.TabIndex = 8;
+            this.DelComboBox1.SelectedIndexChanged += new System.EventHandler(this.DelComboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(298, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Выберите комплект для удаления";
             // 
             // AdminToolsForm
             // 
@@ -200,9 +258,9 @@
             this.Text = "AdminToolsForm";
             this.AddPanel.ResumeLayout(false);
             this.AddPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
             this.DelPanel.ResumeLayout(false);
             this.DelPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -224,5 +282,10 @@
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.TextBox opisTextBox;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button DelButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox DelComboBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Autosalon/AdminToolsForm.cs
+++ Autosalon/AdminToolsForm.cs
@@ -22,6 +22,12 @@ namespace Autosalon
             {
                 carComboBox.Items.Add(car_list[i] + ". " + car_list[i+1]);
             }
+
+            List<string> tool_list = SQLClass.mySelect("SELECT id, name FROM complect");
+            for (int i = 0; i < tool_list.Count; i += 2)
+            {
+                DelComboBox1.Items.Add(tool_list[i] + ". " + tool_list[i + 1]);
+            }
         }
 
         string FileName = "";
@@ -65,6 +71,24 @@ namespace Autosalon
             else
             {
                 Close();
+            }
+        }
+
+        private void DelComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = DelComboBox1.Text;
+        }
+
+        private void DelButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите удалить выбранный объект", "Удаление объекта", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                string id = DelComboBox1.Text.Split(new string[] { ". " }, StringSplitOptions.None)[0];
+                SQLClass.myUpdate("DELETE FROM complect WHERE id = '" + id + "'");
+                MessageBox.Show("Удалено");
+                DelComboBox1.Text = "";
+                textBox1.Text = "";
             }
         }
     }
