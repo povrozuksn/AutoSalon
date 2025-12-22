8d334ad4e314c2b1c8b793fcfcca875bccc4c707
povser75@outlook.com <povser75@outlook.com>
2025-12-22 19:14:17 +0400

    Кнопка удаления на общей форме

--- Autosalon/InfoForm.cs
+++ Autosalon/InfoForm.cs
@@ -28,17 +28,26 @@ namespace Autosalon
                 {
                     Label lbl_id = new Label();
                     lbl_id.Location = new Point(20, y);
-                    lbl_id.Size = new Size(30, 15);
+                    lbl_id.Size = new Size(30, 20);
                     lbl_id.Text = list[i];
+                    lbl_id.Tag = list[i];
                     InfoPanel.Controls.Add(lbl_id);
 
                     Label lbl_name = new Label();
                     lbl_name.Location = new Point(55, y);
-                    lbl_name.Size = new Size(200, 15);
+                    lbl_name.Size = new Size(200, 20);
                     lbl_name.Text = list[i+1];
                     InfoPanel.Controls.Add(lbl_name);
 
-                    y += 20;
+                    Button btn_delete = new Button();
+                    btn_delete.Location = new Point(300, y);
+                    btn_delete.Size = new Size(100, 25);
+                    btn_delete.Font = new Font("Microsoft Sans Serif", 7);
+                    btn_delete.Text = "Удалить";
+                    btn_delete.Click += new EventHandler(DeleteCar);
+                    InfoPanel.Controls.Add(btn_delete);
+
+                    y += 30;
                 }
             }
 
@@ -100,5 +109,21 @@ namespace Autosalon
                 }
             }
         }
+
+        private void DeleteCar (object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int y = btn.Location.Y;
+
+            foreach (Control ctrl in InfoPanel.Controls)
+            {
+                if(ctrl.Location == new Point(20, y))
+                {
+                    SQLClass.myUpdate("DELETE FROM cars WHERE id = '" + ctrl.Tag + "'");
+                    MessageBox.Show("Удалено");
+                }
+            }
+        }
+
     }
 }
